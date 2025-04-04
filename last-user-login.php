ca482d8b97c385fbb3a3e6433b3d29caa0126788
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2025-04-04 10:09:47 +0330

    adding header requirment

---
+++ last-user-login.php
@@ -0,0 +1,15 @@
+/*
+ * Plugin Name:       last user login
+ * Plugin URI:        https://github.com/farzane-na/last-user-login
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Farzane Nazmabadi
+ * Author URI:        https://farzanenazmabadi.ir/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/farzane-na/last-user-login
+ * Text Domain:       last-user-login
+ * Domain Path:       /languages
+ */
